6fbfe76b2f0bd4d6674a772e4b5a5bb4c6d3cf1b
Nick Fung <nlsfung@cs.toronto.edu>
2019-09-09 11:23:31 -0400

    Commented out the checks for datetime manipulation.

--- rim/TemporalDataTypes.rkt
+++ rim/TemporalDataTypes.rkt
@@ -224,24 +224,24 @@
    (define (on-schedule? self dt)
      (on-schedule-rec? self dt schedule-unwind))])
 
-; Symbolic constants for checking properties of date-time manipulation.
-(define-symbolic dt-year dt-month dt-day dt-hour dt-minute dt-second integer?)
-(define-symbolic dur-day dur-hour dur-minute dur-second integer?)
-(define dt-sym (datetime dt-year dt-month dt-day dt-hour dt-minute dt-second))
-(define dur-sym (duration dur-day dur-hour dur-minute dur-second))
-
-; Check that date-times can be normalized after adding/subtracting 31 days max.
-(define (verify-normalize dt-sym dur-sym)
-  (verify #:assume (assert (and (normalized? dt-sym)
-                                (dur>? dur-sym (duration -31 0 0 0))
-                                (dur<? dur-sym (duration  31 0 0 0))))
-          #:guarantee (assert (normalized? (dt+ dt-sym dur-sym)))))
-
-; Check that date-time manipulations are reversible.
-; Tested with datetime-unwind set to 0.
-(define (verify-reversible dt-sym dur-sym)
-  (verify #:assume (assert (and (= dt-year 2000)
-                                (normalized? dt-sym)
-                                (dur>? dur-sym (duration -3 0 0 0))
-                                (dur<? dur-sym (duration 3 0 0 0))))
-          #:guarantee (assert (dt=? dt-sym (dt- (dt+ dt-sym dur-sym) dur-sym)))))
+;; Symbolic constants for checking properties of date-time manipulation.
+;(define-symbolic dt-year dt-month dt-day dt-hour dt-minute dt-second integer?)
+;(define-symbolic dur-day dur-hour dur-minute dur-second integer?)
+;(define dt-sym (datetime dt-year dt-month dt-day dt-hour dt-minute dt-second))
+;(define dur-sym (duration dur-day dur-hour dur-minute dur-second))
+;
+;; Check that date-times can be normalized after adding/subtracting 31 days max.
+;(define (verify-normalize dt-sym dur-sym)
+;  (verify #:assume (assert (and (normalized? dt-sym)
+;                                (dur>? dur-sym (duration -31 0 0 0))
+;                                (dur<? dur-sym (duration  31 0 0 0))))
+;          #:guarantee (assert (normalized? (dt+ dt-sym dur-sym)))))
+;
+;; Check that date-time manipulations are reversible.
+;; Tested with datetime-unwind set to 0.
+;(define (verify-reversible dt-sym dur-sym)
+;  (verify #:assume (assert (and (= dt-year 2000)
+;                                (normalized? dt-sym)
+;                                (dur>? dur-sym (duration -3 0 0 0))
+;                                (dur<? dur-sym (duration 3 0 0 0))))
+;          #:guarantee (assert (dt=? dt-sym (dt- (dt+ dt-sym dur-sym) dur-sym)))))
