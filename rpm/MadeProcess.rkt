8ae8574f8954832d7cad4536694271d8c6d59f71
Nick Fung <nlsfung@cs.toronto.edu>
2019-10-03 13:53:15 -0400

    Updated check for updating the control state of MADE processes.

--- rpm/MadeProcess.rkt
+++ rpm/MadeProcess.rkt
@@ -105,74 +105,79 @@
     
     (make-copy made-proc new-state)))
 
-; Symbolic constants for verifying update data state and update control state
-; on a sample MADE process.
-(struct sample-process made-process ()
-  #:transparent
-  #:methods gen:made-proc
-  [(define (execute self in-data datetime)
-     (gen-proc-execute self in-data datetime))
-
-   (define (update-data-state self in-data)
-     (gen-proc-update-data-state self in-data))
-
-   (define (generate-data self datetime)
-     (gen-proc-generate-data (lambda (d-state dt) #f) self datetime))
-   
-   (define (update-control-state self in-data datetime)
-     (gen-proc-update-control-state self in-data datetime))
-
-   (define (make-copy self elem)
-     (let ([id (made-process-id self)]
-           [d-state (if (list? elem)
-                        elem
-                        (made-process-data-state self))]
-           [c-state (if (control-state? elem)
-                        elem
-                        (made-process-control-state self))]
-           [p-flag (made-process-proxy-flag self)])
-       
-       (sample-process id d-state c-state p-flag)))])   
-
-(define-symbolic p-id integer?)
-(define-symbolic p-data integer?)
-(define-symbolic p-sched p-stat integer?)
-
-(define a-proc (sample-process p-id (list p-data) (control-state p-sched p-stat) #t))
-
-(define-symbolic in1 in2 integer?)
-(define in-data (list in1 in2))
-
-(define new-data-proc (update-data-state a-proc in-data))
-
-(define-symbolic t-id integer?)
-(define-symbolic v-dt-h v-dt-min v-dt-s integer?)
-(define-symbolic c-inst-sched c-inst-stat integer?)
-(define-symbolic c-proxy boolean?)
-
-(define v-dt (datetime 2019 9 18 v-dt-h v-dt-min v-dt-s))
-(define c-inst (control-instruction c-proxy t-id v-dt c-inst-sched c-inst-stat))
-
-(define-symbolic dt-h dt-min dt-s integer?)
-(define cur-dt (datetime 2019 9 18 dt-h dt-min dt-s))
-
-(define new-ctrl-proc (update-control-state a-proc (list c-inst) cur-dt))
-
-; Verify implementation of update data state.
-(define (verify-update-data-state)
-  (verify (assert (implies (< (length (made-process-data-state
-                                       new-data-proc)) 3)
-                           (or (eq? p-data in1)
-                               (eq? p-data in2)
-                               (eq? in1 in2))))))
-
-; Verify implementation of update control state.
-(define (verify-update-control-state)
-  (verify #:assume (assert (and (normalized? v-dt)
-                                (normalized? cur-dt)))
-          #:guarantee (assert (implies (eq? a-proc new-ctrl-proc)
-                                       (or c-proxy
-                                           (not (dt=? v-dt cur-dt))
-                                           (not (= p-id t-id))
-                                           (and (= p-sched c-inst-sched)
-                                                (= p-stat c-inst-stat)))))))
+;; Symbolic constants for verifying update data state and update control state
+;; on a sample MADE process.
+;(struct sample-process made-process ()
+;  #:transparent
+;  #:methods gen:made-proc
+;  [(define (execute self in-data datetime)
+;     (gen-proc-execute self in-data datetime))
+;
+;   (define (update-data-state self in-data)
+;     (gen-proc-update-data-state self in-data))
+;
+;   (define (generate-data self datetime)
+;     (gen-proc-generate-data (lambda (d-state dt) #f) self datetime))
+;   
+;   (define (update-control-state self in-data datetime)
+;     (gen-proc-update-control-state self in-data datetime))
+;
+;   (define (make-copy self elem)
+;     (let ([id (made-process-id self)]
+;           [d-state (if (list? elem)
+;                        elem
+;                        (made-process-data-state self))]
+;           [c-state (if (control-state? elem)
+;                        elem
+;                        (made-process-control-state self))]
+;           [p-flag (made-process-proxy-flag self)])
+;       
+;       (sample-process id d-state c-state p-flag)))])   
+;
+;(define-symbolic p-id integer?)
+;(define-symbolic p-data integer?)
+;(define-symbolic p-sched p-stat integer?)
+;
+;(define a-proc (sample-process p-id (list p-data) (control-state p-sched p-stat) #t))
+;
+;(define-symbolic in1 in2 integer?)
+;(define in-data (list in1 in2))
+;
+;(define new-data-proc (update-data-state a-proc in-data))
+;
+;(define-symbolic t-id integer?)
+;(define-symbolic v-dt-h v-dt-min v-dt-s integer?)
+;(define-symbolic c-inst-sched c-inst-stat integer?)
+;(define-symbolic c-inst-sched-void c-inst-stat-void boolean?)
+;(define-symbolic c-proxy boolean?)
+;
+;(define v-dt (datetime 2019 9 18 v-dt-h v-dt-min v-dt-s))
+;(define c-inst (control-instruction c-proxy t-id v-dt
+;                                    (if c-inst-sched-void (void) c-inst-sched)
+;                                    (if c-inst-stat-void (void) c-inst-stat)))
+;
+;(define-symbolic dt-h dt-min dt-s integer?)
+;(define cur-dt (datetime 2019 9 18 dt-h dt-min dt-s))
+;
+;(define new-ctrl-proc (update-control-state a-proc (list c-inst) cur-dt))
+;
+;; Verify implementation of update data state.
+;(define (verify-update-data-state)
+;  (verify (assert (implies (< (length (made-process-data-state
+;                                       new-data-proc)) 3)
+;                           (or (eq? p-data in1)
+;                               (eq? p-data in2)
+;                               (eq? in1 in2))))))
+;
+;; Verify implementation of update control state.
+;(define (verify-update-control-state)
+;  (verify #:assume (assert (and (normalized? v-dt)
+;                                (normalized? cur-dt)))
+;          #:guarantee (assert (implies (eq? a-proc new-ctrl-proc)
+;                                       (or c-proxy
+;                                           (not (dt=? v-dt cur-dt))
+;                                           (not (= p-id t-id))
+;                                           (and (or (= p-sched c-inst-sched)
+;                                                    c-inst-sched-void)
+;                                                (or (= p-stat c-inst-stat)
+;                                                    c-inst-stat-void)))))))
