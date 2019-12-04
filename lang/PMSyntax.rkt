97351cbcda220bf0d1f55b5885284cf2c3d1f138
Nick Fung <nlsfung@cs.toronto.edu>
2019-12-04 11:57:33 -0500

    Added syntax for specifying Monitoring processes that output observed events.

--- lang/PMSyntax.rkt
+++ lang/PMSyntax.rkt
@@ -8,6 +8,65 @@
 
 ; This file contains the syntax for specifying new MADE processes.
 
+; define-monitoring creates a new Monitoring process.
+; It requires the following inputs:
+; 1) An indicator whether the process is for a property or an event.
+; 1) An identifier for the new process.
+; 2) A boolean indicating whether the process is a proxy or not.
+; 3) For processes that generate observed properties:
+;    a) The time window for the process.
+;    b) The value function for the process.
+; 4) For processes that generate observed events:
+;    a) The trigger (i.e. time window and predicate) for the start of the event.
+;    b) The trigger for the end of the event.
+; 5) The output type of the process.
+(define-syntax (define-monitoring stx)
+  (syntax-case stx ()
+    [(define-monitoring #:property id proxy (... duration) (... lambda) output-type)
+     (begin
+       (raise-if-not-identifier #'id stx)
+       (raise-if-not-boolean #'proxy stx)
+       (raise-if-not-duration #'duration stx)
+       (raise-if-not-lambda #'lambda 1 stx)
+       (raise-if-not-identifier #'output-type stx)
+       #'(struct id monitoring-process ()
+           #:transparent
+           #:methods gen:monitoring
+           [(define (monitoring-process-output-specification self)
+              (property-specification duration lambda))
+            (define (monitoring-process-output-type self) output-type)
+            (define (monitoring-process-proxy-flag self) proxy)]
+
+           #:methods gen:typed
+           [(define/generic super-valid? valid?)
+            (define (get-type self) id)
+            (define (valid? self)
+              (and (valid-spec? self)
+                   (super-valid? (made-process (made-process-data-state self)
+                                               (made-process-control-state self)))))]))]
+
+    [(define-monitoring #:event id proxy (... start) (... end) output-type)
+     (begin
+       (raise-if-not-identifier #'id stx)
+       (raise-if-not-boolean #'proxy stx)
+       (raise-if-not-identifier #'output-type stx)
+       #'(struct id monitoring-process ()
+           #:transparent
+           #:methods gen:monitoring
+           [(define (monitoring-process-output-specification self)
+              (event-specification (define-event-trigger start)
+                                   (define-event-trigger end)))
+            (define (monitoring-process-output-type self) output-type)
+            (define (monitoring-process-proxy-flag self) proxy)]
+
+           #:methods gen:typed
+           [(define/generic super-valid? valid?)
+            (define (get-type self) id)
+            (define (valid? self)
+              (and (valid-spec? self)
+                   (super-valid? (made-process (made-process-data-state self)
+                                               (made-process-control-state self)))))]))]))
+
 ; Helper function for raising syntax error if input is not an identifier or a
 ; list of identifiers. Returns the syntax object for void if the check succeeds.
 (define-for-syntax (raise-if-not-identifier stx src-stx)
@@ -84,38 +143,14 @@
                         #'(void))])]
     [(... _) (raise-syntax-error #f "lambda expression expected." src-stx stx)]))
 
-; define-monitoring creates a new Monitoring process.
-; It requires the following inputs:
-; 1) An identifier for the new process.
-; 2) A boolean indicating whether the process is a proxy or not.
-; 3) For processes that generate observed properties:
-;    a) The time window for the process.
-;    b) The value function for the process.
-; 4) For processes that generate observed events:
-;    a) The trigger (i.e. time window and predicate) for the start of the event.
-;    b) The trigger for the end of the event.
-; 5) The output type of the process.
-(define-syntax (define-monitoring stx)
+; Helper function for parsing the syntax of event triggers.
+; Returns the syntax object for void if the check succeeds.
+(define-syntax (define-event-trigger stx)
   (syntax-case stx ()
-    [(define-monitoring id proxy (... duration) (... lambda) output-type)
+    [(define-event-trigger (event-trigger (... window) (... predicate)))
      (begin
-       (raise-if-not-identifier #'id stx)
-       (raise-if-not-boolean #'proxy stx)
-       (raise-if-not-duration #'duration stx)
-       (raise-if-not-lambda #'lambda 1 stx)
-       (raise-if-not-identifier #'output-type stx)
-       #'(struct id monitoring-process ()
-           #:transparent
-           #:methods gen:monitoring
-           [(define (monitoring-process-output-specification self)
-              (property-specification duration lambda))
-            (define (monitoring-process-output-type self) output-type)
-            (define (monitoring-process-proxy-flag self) proxy)]
-
-           #:methods gen:typed
-           [(define/generic super-valid? valid?)
-            (define (get-type self) id)
-            (define (valid? self)
-              (and (valid-spec? self)
-                   (super-valid? (made-process (made-process-data-state self)
-                                               (made-process-control-state self)))))]))]))
+       (raise-if-not-duration #'window stx)
+       (raise-if-not-lambda #'predicate 1 stx)
+       (if (not (eq? (syntax->datum #'event-trigger) 'event-trigger))
+           (raise-syntax-error #f "event trigger expected." stx #'event-trigger)
+           #'(event-trigger window predicate)))]))
