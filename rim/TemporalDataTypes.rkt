4ce4f5c6c9a839b0eedef1573dbf8a5112d6f87b
Nick Fung <nlsfung@cs.toronto.edu>
2020-02-14 15:29:31 -0500

    Moved invariants for temporal data types to a separate file.

--- rim/TemporalDataTypes.rkt
+++ rim/TemporalDataTypes.rkt
@@ -3,8 +3,7 @@
 (require (only-in "./BasicDataTypes.rkt" gen:typed get-type valid?))
 
 (provide (struct-out datetime)
-         dt=? dt- dt+ dt>? dt<?
-         dt<=? dt>=?
+         dt=? dt- dt+ dt>? dt<? dt<=? dt>=?
          normalized? dt-between? datetime->number)
 (provide (struct-out schedule) on-schedule?)
 (provide (struct-out duration) dur- dur+ dur<? dur>? dur=? duration->second)
@@ -283,25 +282,3 @@
                             (schedule-pattern self))
                       #f))]
            [else (on-schedule-rec? self dt schedule-unwind)]))])
-
-;; Symbolic constants for checking properties of date-time manipulation.
-;(define-symbolic dt-year dt-month dt-day dt-hour dt-minute dt-second integer?)
-;(define-symbolic dur-day dur-hour dur-minute dur-second integer?)
-;(define dt-sym (datetime dt-year dt-month dt-day dt-hour dt-minute dt-second))
-;(define dur-sym (duration dur-day dur-hour dur-minute dur-second))
-;
-;; Check that date-times can be normalized after adding/subtracting 31 days max.
-;(define (verify-normalize dt-sym dur-sym)
-;  (verify #:assume (assert (and (normalized? dt-sym)
-;                                (dur>? dur-sym (duration -31 0 0 0))
-;                                (dur<? dur-sym (duration  31 0 0 0))))
-;          #:guarantee (assert (normalized? (dt+ dt-sym dur-sym)))))
-;
-;; Check that date-time manipulations are reversible.
-;; Tested with datetime-unwind set to 0.
-;(define (verify-reversible dt-sym dur-sym)
-;  (verify #:assume (assert (and (= dt-year 2000)
-;                                (normalized? dt-sym)
-;                                (dur>? dur-sym (duration -1 0 0 0))
-;                                (dur<? dur-sym (duration 1 0 0 0))))
-;          #:guarantee (assert (dt=? dt-sym (dt- (dt+ dt-sym dur-sym) dur-sym)))))
